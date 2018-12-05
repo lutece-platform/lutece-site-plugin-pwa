f63c7306b03c3cff751363a4ce8749590ecb1969
pierrelevy <pierrelevy@users.noreply.github.com>
2018-12-05 16:56:31 +0100

    PWA-1 : Initialization - properties naming improvements and ordering

--- src/sql/plugins/pwa/plugin/init_db_pwa.sql
+++ src/sql/plugins/pwa/plugin/init_db_pwa.sql
@@ -1,14 +1,15 @@
 INSERT INTO 
 	core_datastore 
 VALUES
-	("pwa.site_property.name", ""),
-	("pwa.site_property.short_name", ""),
-	("pwa.site_property.start.url", ""),
-	("pwa.site_property.display", "fullscreen"),
-	("pwa.site_property.background.color", "#ffffff"),
-	("pwa.site_property.theme.color", "#0000FF"),
-	("pwa.site_property.scope", "/"),
-	("pwa.site_property.description", ""),
-	("pwa.site_property.orientation", "portrait"),
-	("pwa.site_property.icon.name", "launcher-icon"),
-	("pwa.site_property.fo.start.url", "Portal.jsp");
+	("pwa.site_property.01.name", ""),
+	("pwa.site_property.02.short_name", ""),
+	("pwa.site_property.03.description", ""),
+	("pwa.site_property.04.display", "fullscreen"),
+	("pwa.site_property.05.orientation", "portrait"),
+	("pwa.site_property.06.start.url", ""),
+	("pwa.site_property.07.scope", "/"),
+	("pwa.site_property.08.fo.start.url", "Portal.jsp"),
+	("pwa.site_property.09.icon.name", "launcher-icon"),
+	("pwa.site_property.10.background.color", "#ffffff"),
+	("pwa.site_property.11.theme.color", "#0000FF");
+
